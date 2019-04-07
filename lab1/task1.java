92930ae05be2c346930ad6596d75b2879ced5351
FatimaYousif <49322171+FatimaYousif@users.noreply.github.com>
2019-04-07 12:15:42 +0500

    Add files via upload

---
+++ lab1/task1.java
@@ -0,0 +1,31 @@
+class task1
+{
+public static void main(String args[])
+{
+String name="Fatima";
+int tot_marks=1100;
+short obt_marks=915;
+double percent=83.18;
+char grade=65;
+boolean status=true;
+System.out.print("name: ");
+System.out.println(name);
+System.out.print("total marks= ");
+System.out.println(tot_marks);
+System.out.print("obtained marks= ");
+System.out.println(obt_marks);
+System.out.print("percentage= ");
+System.out.print(percent);
+System.out.println("%");
+System.out.print("grade= ");
+System.out.print(grade);
+System.out.println(1);
+System.out.print("status= ");
+if(status==true) {
+System.out.println("pass");
+}
+else{
+System.out.println("fail");
+}
+}
+}
---
+++ lab1/task2.java
@@ -0,0 +1,15 @@
+class task
+{
+public static void main(String args[])
+{
+double a=50.25;
+long b=(int)a;
+double c=a-b;
+System.out.print("The number itself= ");
+System.out.println(a);
+System.out.print("The integral part= ");
+System.out.println(b);
+System.out.print("The fractional part= ");
+System.out.println(c);
+}
+}
